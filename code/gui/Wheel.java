e72c47516acf025b1cb46ce9c1ad0a92869dd383
Cake <therealcakeoftruth@gmail.com>
2024-07-21 19:48:08 -0400

    Add text to wheel

--- code/gui/Wheel.java
+++ code/gui/Wheel.java
@@ -1,6 +1,8 @@
 package code.gui;
 
+import java.awt.Color;
 import java.awt.Container;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Arc2D;
@@ -10,10 +12,12 @@ import javax.swing.JComponent;
 import code.util.GraphicsUtil;
 
 public class Wheel extends JComponent {
-    Container parent;
-    boolean autoTransform = false;
-    double x, y, r;
-    int n = 50;
+    private final Font font = new Font("Comic Sans MS", Font.LAYOUT_LEFT_TO_RIGHT, 19);
+    
+    private Container parent;
+    private boolean autoTransform = false;
+    private double x, y, r;
+    private int n = 5;
 
     public Wheel() {}
 
@@ -31,8 +35,8 @@ public class Wheel extends JComponent {
 
     @Override
     public void paintComponent(Graphics g) {
-        parent = getParent();
-        if (parent == null) {return;}
+        g.setFont(font);
+        if (parent != getParent()) {parent = getParent(); if(parent == null) {return;}}
         setSize(parent.getSize());
         if (autoTransform) {recalcTransforms();}
         
@@ -40,13 +44,22 @@ public class Wheel extends JComponent {
         Graphics2D g2d = (Graphics2D) g;
 
         double dt = (double) 360 / n;
-        double startAngle = 90 - (dt / 2);
+        double startAngle = -(dt / 2);
 
         for (int i = 0; i < n; i++) {
+            // The arc
             Arc2D arc = new Arc2D.Double(x - r, y - r, 2*r, 2*r, startAngle, dt, Arc2D.PIE);
             g2d.setColor(GraphicsUtil.colorFilter(GraphicsUtil.randomColor()));
             g2d.fill(arc);
-            startAngle += dt;
+
+            // The text (only render within the arc slice, then reset the clipping region so the other slices can be drawn)
+            g2d.clip(arc);
+            g2d.setColor(new Color(0xFFFFFF));
+            g2d.drawString("Sample text", (int) (x + r/3), (int) (y + 4.5));
+            g2d.setClip(getBounds());
+
+            // Apply rotation about the center
+            g2d.rotate(-dt*Math.PI/180, x, y);
         }
         
         /* 
